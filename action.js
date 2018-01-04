2126e883f7d7858e04b06d6170a9e5acd5d82fb1
Jerliyah <jerliyahcraig@gmail.com>
2018-01-04 13:33:27 -0600

    functionality first draft

---
+++ action.js
@@ -0,0 +1,38 @@
+var inputs = document.querySelectorAll('input');
+
+// Set defaults (doing so in html doesn't work on refresh for some reason)
+inputs.forEach( (input) => {
+    if( input.type == "range" ) {
+        input.value = 0;
+    }
+    else if( input.type == "color" ) {
+        input.value = "#ffffff";
+    }
+})
+
+
+function updater() {
+    let ball = this.parentNode.nextElementSibling;
+
+    switch( this.name ) {
+        case 'color': 
+            ball.style.setProperty('background', this.value)
+            break;
+        
+        case 'shapeshift':
+            ball.style.setProperty('border-radius', this.value + "%");
+            break;
+
+        case 'levitate':
+            ball.style.setProperty('bottom', this.value + "%")
+            break;
+
+    }
+}
+
+
+inputs.forEach( (input) => {
+    input.addEventListener('change', updater);
+    // input.addEventListener('mousemove', updater)
+})
+
